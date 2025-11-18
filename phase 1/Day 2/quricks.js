2f50ba5da2146a158650548a4eb1fb7f90495d7f
MOHAMMED RAFIQUIE AHMED <158809811+RafiquieAhmed@users.noreply.github.com>
2025-11-18 23:39:43 +0530

    Add files via upload

---
+++ phase 1/Day 2/quricks.js
@@ -0,0 +1,5 @@
+console.log(typeof []);    //object
+console.log(typeof null);  //object
+console.log(typeof {});    //object
+console.log(typeof function () {}); //functions
+console.log(typeof NaN); //number
